ddbb7b5e5926c2aa9f771805b23f516f43e58847
Ethel C <cofresi.ethel@gmail.com>
2018-08-30 20:42:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,6 +69,10 @@ class Student
     end.first
   end
   
-  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 
 end
