28224f0ebc93849e41299bff4cb3543c3ed341d1
Ethel C <cofresi.ethel@gmail.com>
2018-08-30 20:39:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,23 @@ class Student
     @grade = grade
   end
   
-  
+    def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+
+    DB[:conn].execute(sql)
+  end
+
 
 end
